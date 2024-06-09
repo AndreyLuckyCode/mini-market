e4a3227439a3c8568ef0ed756554b14086cc000d
Andrey Trubilo <andreytrubilo@mail.ru>
2024-06-10 00:24:18 +0300

    error message file exceptions in REstClientProductRestClient todo - custom exceptions

--- manager-app/src/main/java/andrey/code/manager/client/RestClientProductRestClient.java
+++ manager-app/src/main/java/andrey/code/manager/client/RestClientProductRestClient.java
@@ -7,18 +7,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RestClientProductRestClient implements ProductRestClient{
 
     RestClient restClient;
+
+    private static final String URI_CONST = "/api/v1/products/{productId}";
 
     private static final ParameterizedTypeReference<List<ProductDTO>> PRODUCT_DTO_LIST_TYPE_REFERENCE
             = new ParameterizedTypeReference<List<ProductDTO>>() {};
@@ -27,13 +32,18 @@ public class RestClientProductRestClient implements ProductRestClient{
     @Override
     public ResponseEntity<String> createProduct(@RequestBody ProductPayload payload) {
 
-        return restClient
-                .post()
-                .uri("/api/v1/products")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(payload)
-                .retrieve()
-                .toEntity(String.class);
+        try {
+            return restClient
+                    .post()
+                    .uri("/api/v1/products")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(payload)
+                    .retrieve()
+                    .toEntity(String.class);
+        } catch (HttpClientErrorException.BadRequest exception) {
+            ProblemDetail problemDetail = exception.getResponseBodyAs(ProblemDetail.class);
+            throw new BadRequestException((List<String>) problemDetail.getProperties().get("errors"));
+        }
     }
 
     @Override
@@ -50,37 +60,48 @@ public class RestClientProductRestClient implements ProductRestClient{
 
     @Override
     public ResponseEntity<ProductDTO> getProductById (@PathVariable ("productId") Long id) {
-//todo Обработчик исключений
-        ProductDTO product =
-                restClient
-                        .get()
-                        .uri("/api/v1/products/{productId}", id)
-                        .retrieve()
-                        .body(ProductDTO.class);
+        try {
+            ProductDTO product =
+                    restClient
+                            .get()
+                            .uri(URI_CONST, id)
+                            .retrieve()
+                            .body(ProductDTO.class);
 
-        return ResponseEntity.ok(product);
+            return ResponseEntity.ok(product);
+        } catch (HttpClientErrorException.NotFound exception) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Override
     public ResponseEntity<String> updateProduct(@PathVariable ("productId") Long id,
                                                 @RequestBody ProductPayload payload) {
 
-        return restClient
-                .patch()
-                .uri("/api/v1/products/{productId}", id)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(payload)
-                .retrieve()
-                .toEntity(String.class);
+        try {
+            return restClient
+                    .patch()
+                    .uri(URI_CONST, id)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(payload)
+                    .retrieve()
+                    .toEntity(String.class);
+        } catch (HttpClientErrorException.BadRequest exception) {
+            ProblemDetail problemDetail = exception.getResponseBodyAs(ProblemDetail.class);
+            throw new BadRequestException((List<String>) problemDetail.getProperties().get("errors"));
+        }
     }
 
     @Override
     public ResponseEntity<String> deleteProduct(@PathVariable ("productId") Long id) {
-
-        return restClient
-                .delete()
-                .uri("/api/v1/products/{productId}", id)
-                .retrieve()
-                .toEntity(String.class);
+        try {
+            return restClient
+                    .delete()
+                    .uri(URI_CONST, id)
+                    .retrieve()
+                    .toEntity(String.class);
+        } catch (HttpClientErrorException.NotFound exception) {
+            throw new NoSuchElementException(exception);
+        }
     }
 }
