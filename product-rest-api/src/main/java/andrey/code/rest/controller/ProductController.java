e7e6a4520e0f202fc566594a8b4d9e9bd418ce77
Andrey Trubilo <andreytrubilo@mail.ru>
2024-06-13 02:29:51 +0300

    NoSuchElementException (getById method) REST API side

--- product-rest-api/src/main/java/andrey/code/rest/controller/ProductController.java
+++ product-rest-api/src/main/java/andrey/code/rest/controller/ProductController.java
@@ -1,16 +1,21 @@
 package andrey.code.rest.controller;
 
 import andrey.code.rest.controller.payload.ProductPayload;
+import andrey.code.rest.entity.ProductEntity;
 import andrey.code.rest.mapper.ProductDTO;
 import andrey.code.rest.service.ProductService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
@@ -19,6 +24,7 @@ import java.util.Optional;
 @RequestMapping("api/v1")
 public class ProductController {
 
+    MessageSource messageSource;
     ProductService productService;
 
     private static final String CREATE_PRODUCT = "/products";
@@ -26,6 +32,13 @@ public class ProductController {
     private static final String GET_PRODUCTS_LIST = "/products";
     private static final String UPDATE_PRODUCT = "/products/{productId}";
     private static final String DELETE_PRODUCT = "/products/{productId}";
+
+    @ModelAttribute("product")
+    public ProductDTO getProduct(@PathVariable("productId") Long productId){
+
+        return productService.getProductById(productId).orElseThrow(()
+                -> new NoSuchElementException("errors.404.not_found"));
+    }
 
     @PostMapping(CREATE_PRODUCT)
     public ResponseEntity<String> createProduct(@RequestBody ProductPayload payload){
@@ -54,5 +67,14 @@ public class ProductController {
     @DeleteMapping(DELETE_PRODUCT)
     public ResponseEntity<String> deleteProduct(@PathVariable("productId") Long id){
         return productService.deleteProduct(id);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ProblemDetail> handleNoSuchElementException(NoSuchElementException exception,
+                                                                      Locale locale) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                        this.messageSource.getMessage(exception.getMessage(), new Object[0],
+                                exception.getMessage(), locale)));
     }
 }
