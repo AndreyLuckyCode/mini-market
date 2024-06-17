a7bb9705eb1d34bdea7fedefaf7b9e253639a59d
Andrey Trubilo <andreytrubilo@mail.ru>
2024-06-17 16:47:00 +0300

    Security between 2 modules request of 2 module has log & pass to use rest api of 1 module

--- manager-app/src/main/java/andrey/code/manager/config/ClientBeans.java
+++ manager-app/src/main/java/andrey/code/manager/config/ClientBeans.java
@@ -4,6 +4,7 @@ import andrey.code.manager.client.RestClientProductRestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,9 +12,13 @@ public class ClientBeans {
 
     @Bean
     public RestClientProductRestClient productsRestClient(
-            @Value("${market.services.rest.uri:http://localhost:8081}") String catalogueBaseUri) {
+            @Value("${market.services.rest.uri:http://localhost:8081}") String catalogueBaseUri,
+            @Value("${market.services.rest.username:rest_service_user}") String restServiceUsername,
+            @Value("${market.services.rest.password:password}") String restServicePassword) {
         return new RestClientProductRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(
+                        new BasicAuthenticationInterceptor(restServiceUsername, restServicePassword))
                 .build());
     }
 }
---
+++ manager-app/src/main/java/andrey/code/manager/config/SecurityBeans.java
@@ -0,0 +1,22 @@
+package andrey.code.manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityBeans {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        return http
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
