6b946edc28366a8ef76457d400e0520d92d3f409
Andrey Trubilo <andreytrubilo@mail.ru>
2024-06-26 19:38:47 +0300

    @Operation swagger request config in the ProductController (product-rest-api module)
    SpringDocBeans, Security beans allows requests from swagger for ui tests

--- product-rest-api/src/main/java/andrey/code/rest/controller/ProductController.java
+++ product-rest-api/src/main/java/andrey/code/rest/controller/ProductController.java
@@ -3,11 +3,19 @@ package andrey.code.rest.controller;
 import andrey.code.rest.controller.payload.ProductPayload;
 import andrey.code.rest.mapper.ProductDTO;
 import andrey.code.rest.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.StringToClassMapItem;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,11 +41,72 @@ public class ProductController {
     private static final String NOT_FOUND_ERROR = "errors.404.not_found";
 
     @PostMapping(CREATE_PRODUCT)
+    @Operation(
+            security = @SecurityRequirement(name = "keycloak"),
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    type = "object",
+                                    properties = {
+                                            @StringToClassMapItem(key = "name", value = String.class),
+                                            @StringToClassMapItem(key = "price", value = Integer.class),
+                                            @StringToClassMapItem(key = "somePrivateInfo", value = String.class)
+                                    }
+                            )
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            headers = @Header(name = "Content-Type", description = "Тип данных"),
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                            schema = @Schema(
+                                                    type = "object",
+                                                    properties = {
+                                                            @StringToClassMapItem(key = "name", value = String.class),
+                                                            @StringToClassMapItem(key = "price", value = Integer.class),
+                                                    }
+                                            )
+                                    )
+                            }
+                    )
+            })
     public ResponseEntity<String> createProduct(@RequestBody ProductPayload payload){
         return productService.createProduct(payload);
     }
 
     @GetMapping(GET_PRODUCT)
+    @Operation(
+            security = @SecurityRequirement(name = "keycloak"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Успешный запрос",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ProductDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Продукт не найден",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            type = "object",
+                                            properties = {
+                                                    @StringToClassMapItem(key = "message", value = String.class)
+                                            }
+                                    )
+                            )
+                    )
+            }
+    )
     public ResponseEntity<ProductDTO> getProductById(@PathVariable("productId") Long id) {
 
         ProductDTO product = productService.getProductById(id)
@@ -47,12 +116,71 @@ public class ProductController {
     }
 
     @GetMapping(GET_PRODUCTS_LIST)
+    @Operation(
+            security = @SecurityRequirement(name = "keycloak"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Успешный запрос",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            type = "array"
+                                    )
+                            )
+                    )
+            }
+    )
     public ResponseEntity<List<ProductDTO>> getAllProducts(){
 
         return productService.getAllProducts();
     }
 
     @PatchMapping(UPDATE_PRODUCT)
+    @Operation(
+            security = @SecurityRequirement(name = "keycloak"),
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    type = "object",
+                                    properties = {
+                                            @StringToClassMapItem(key = "name", value = String.class),
+                                            @StringToClassMapItem(key = "price", value = String.class),
+                                            @StringToClassMapItem(key = "somePrivateInfo", value = String.class)
+                                    }
+                            )
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Продукт успешно обновлен",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            type = "object",
+                                            properties = {
+                                                    @StringToClassMapItem(key = "message", value = String.class)
+                                            }
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Продукт не найден",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            type = "object",
+                                            properties = {
+                                                    @StringToClassMapItem(key = "message", value = String.class)
+                                            }
+                                    )
+                            )
+                    )
+            }
+    )
     public ResponseEntity<String> updateProduct(@PathVariable("productId") Long id,
                                                 @RequestBody ProductPayload payload) {
 
@@ -63,6 +191,37 @@ public class ProductController {
     }
 
     @DeleteMapping(DELETE_PRODUCT)
+    @Operation(
+            security = @SecurityRequirement(name = "keycloak"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Продукт успешно удален",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            type = "object",
+                                            properties = {
+                                                    @StringToClassMapItem(key = "message", value = String.class)
+                                            }
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Продукт не найден",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            type = "object",
+                                            properties = {
+                                                    @StringToClassMapItem(key = "message", value = String.class)
+                                            }
+                                    )
+                            )
+                    )
+            }
+    )
     public ResponseEntity<String> deleteProduct(@PathVariable("productId") Long id) {
 
         productService.getProductById(id)
